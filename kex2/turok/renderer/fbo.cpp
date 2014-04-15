c519efca6dc415ae1ed12847d38c5fdf95b910f5
Samuel Villarreal <svkaiser@gmail.com>
2014-04-15 00:40:59 -0500

    + fbo module added

---
+++ kex2/turok/renderer/fbo.cpp
@@ -0,0 +1,185 @@
+// Emacs style mode select   -*- C++ -*- 
+//-----------------------------------------------------------------------------
+//
+// Copyright(C) 2014 Samuel Villarreal
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+// 02111-1307, USA.
+//
+//-----------------------------------------------------------------------------
+//
+// DESCRIPTION: Framebuffer objects
+//
+//-----------------------------------------------------------------------------
+
+#include "common.h"
+#include "renderBackend.h"
+#include "fbo.h"
+
+//
+// kexFBO::kexFBO
+//
+
+kexFBO::kexFBO(void) {
+    this->fboId = 0;
+    this->rboId = 0;
+    this->fboTexId = 0;
+    this->bLoaded = false;
+}
+
+//
+// kexFBO::~kexFBO
+//
+
+kexFBO::~kexFBO(void) {
+    Delete();
+}
+
+//
+// kexFBO::InitColorAttachment
+//
+
+void kexFBO::InitColorAttachment(const int attachment, const int width, const int height) {
+    if(bLoaded) {
+        return;
+    }
+    
+    if(attachment < 0 || attachment >= renderBackend.MaxColorAttachments()) {
+        return;
+    }
+    
+    // texture
+    dglGenTextures(1, &fboTexId);
+    dglBindTexture(GL_TEXTURE_2D, fboTexId);
+    dglTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    dglTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    dglTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    dglTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    dglTexImage2D(GL_TEXTURE_2D,
+                  0,
+                  GL_RGBA,
+                  width,
+                  height,
+                  0,
+                  GL_RGBA,
+                  GL_UNSIGNED_BYTE,
+                  0);
+    
+    // renderbuffer
+    dglGenRenderbuffers(1, &rboId);
+    dglBindRenderbuffer(GL_RENDERBUFFER_EXT, rboId);
+    dglRenderbufferStorage(GL_RENDERBUFFER_EXT,
+                           GL_DEPTH_COMPONENT,
+                           width,
+                           height);
+    
+    // framebuffer
+    dglGenFramebuffers(1, &fboId);
+    dglBindFramebuffer(GL_FRAMEBUFFER_EXT, fboId);
+    dglFramebufferTexture2D(GL_FRAMEBUFFER_EXT,
+                            GL_COLOR_ATTACHMENT0_EXT + attachment,
+                            GL_TEXTURE_2D,
+                            fboTexId,
+                            0);
+    
+    dglFramebufferRenderbuffer(GL_FRAMEBUFFER_EXT,
+                               GL_DEPTH_ATTACHMENT_EXT,
+                               GL_RENDERBUFFER_EXT,
+                               rboId);
+    
+    GLenum status = dglCheckFramebufferStatus(GL_FRAMEBUFFER_EXT);
+    
+    if(status != GL_FRAMEBUFFER_COMPLETE_EXT) {
+        common.Warning("kexFBO::Init: frame buffer creation didn't complete\n");
+        bLoaded = false;
+    }
+    else {
+        bLoaded = true;
+    }
+    
+    dglBindFramebuffer(GL_FRAMEBUFFER_EXT, 0);
+    dglBindRenderbuffer(GL_RENDERBUFFER_EXT, 0);
+    dglBindTexture(GL_TEXTURE_2D, 0);
+}
+
+//
+// kexFBO::InitColorAttachment
+//
+
+void kexFBO::InitColorAttachment(const int attachment) {
+    int width = sysMain.VideoWidth();
+    int height = sysMain.VideoHeight();
+    
+    InitColorAttachment(attachment, width, height);
+}
+
+//
+// kexFBO::Delete
+//
+
+void kexFBO::Delete(void) {
+    if(!bLoaded) {
+        return;
+    }
+    
+    if(fboTexId != 0) {
+        dglDeleteTextures(1, &fboTexId);
+        fboTexId = 0;
+    }
+    
+    bLoaded = false;
+}
+
+//
+// kexFBO::Bind
+//
+
+void kexFBO::Bind(void) {
+    if(fboId == renderBackend.glState.currentFBO) {
+        return;
+    }
+    
+    dglBindFramebuffer(GL_FRAMEBUFFER_EXT, fboId);
+    renderBackend.glState.currentFBO = fboId;
+}
+
+//
+// kexFBO::UnBind
+//
+
+void kexFBO::UnBind(void) {
+    if(renderBackend.glState.currentFBO == 0) {
+        return;
+    }
+    
+    dglBindFramebuffer(GL_FRAMEBUFFER_EXT, 0);
+    renderBackend.glState.currentFBO = 0;
+}
+
+//
+// kexFBO::BindImage
+//
+
+void kexFBO::BindImage(void) {
+    int unit = renderBackend.glState.currentUnit;
+    dtexture currentTexture = renderBackend.glState.textureUnits[unit].currentTexture;
+    
+    if(fboTexId == currentTexture) {
+        return;
+    }
+    
+    dglBindTexture(GL_TEXTURE_2D, fboTexId);
+    renderBackend.glState.textureUnits[unit].currentTexture = fboTexId;
+}
